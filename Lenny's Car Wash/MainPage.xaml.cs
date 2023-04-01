7fb86c66272cbe81c387d76e2a6741e5d2b82f39
TaiHPham <taihpham09@gmail.com>
2023-04-01 02:05:51 -0500

    Update MainPage.xaml.cs
    Calculation and displayed using Navigation.PushAsync method. Need to work on the error message instead of using the DisplayAlert method.

--- Lenny's Car Wash/MainPage.xaml.cs
+++ Lenny's Car Wash/MainPage.xaml.cs
@@ -2,37 +2,95 @@
 
 public partial class MainPage : ContentPage
 {
-	int totalCost = 0;
-
 	public MainPage()
 	{
 		InitializeComponent();
-	}
 
-	void SelectWashChoice(object sender, EventArgs e)
-	{
-		if(rdoBasicWash.IsChecked)
-		{
-			totalCost += 10;
-		}
-		else if(rdoPremiumWash.IsChecked)
-		{
-			totalCost += 15;
-		}
-		else if(rdoUltraWash.IsChecked)
-		{
-			totalCost += 20;
-		}
-		else
-		{
-			Console.WriteLine("No options were picked for type of wash!");
-		}
+        submitBtn1.Clicked += CalcAmount;
+    }
 
-		if()
-		{
-			totalCost += 5;
-		}
+    private void CalcAmount(object sender, EventArgs e)
+    {
+        double totalCost = 0.00;
+        bool isWashSelected = false;
+        string error = "Please select a wash";
 
-	}
+        if (rdoBasicWash.IsChecked == true)
+        {
+            totalCost = 10.00;
+            isWashSelected = true;
+        }
+        else if(rdoPremiumWash.IsChecked == true)
+        {
+            totalCost = 15.00;
+            isWashSelected = true;
+        }
+        else if(rdoUltraWash.IsChecked == true)
+        {
+            totalCost = 20.00;
+            isWashSelected = true;
+        }
+
+        if (isWashSelected == false)
+            DisplayAlert("Error: ", error, "OK");
+        else
+        {
+            if (chkAirFresh.IsChecked == true)
+                totalCost += 2.00;
+            if (chkWax.IsChecked == true)
+                totalCost += 5.00;
+            if (chkShine.IsChecked == true)
+                totalCost += 5.00;
+            if (chkVacuum.IsChecked == true)
+                totalCost += 2.00;
+
+            ReceiptPage receiptPage = new ReceiptPage(totalCost);
+            Navigation.PushAsync(receiptPage);
+        }
+    }
+
+    /*public ReceiptPage(List<string> selectedItems, double totalPrice)
+    {
+        InitializeComponent();
+
+        // Add the selected items to the list view
+        foreach (string item in selectedItems)
+        {
+            listView.Items.Add(item);
+        }
+
+        // Display the total price
+        return totalPriceLabel.Text = $"Total Price: {totalPrice:C}";
+    }
+
+    private async void OnGenerateReceiptClicked(object sender, EventArgs e)
+    {
+        // Get the selected items and their prices
+        List<string> selectedItems = new List<string>();
+        List<decimal> selectedPrices = new List<decimal>();
+
+        if (rdoBasicWash.IsChecked)
+        {
+            selectedItems.Add((string)rdoBasicWash.Content);
+            selectedPrices.Add(10.00m);
+        }
+        if (rdoPremiumWash.IsChecked)
+        {
+            selectedItems.Add((string)rdoPremiumWash.Content);
+            selectedPrices.Add(15.00m);
+        }
+        if (rdoUltraWash.IsChecked)
+        {
+            selectedItems.Add((string)rdoUltraWash.Content);
+            selectedPrices.Add(20.00m);
+        }
+
+        // Calculate the total price
+        decimal totalPrice = selectedPrices.Sum();
+
+        // Navigate to the receipt page and pass the selected items and total price as parameters
+        await Navigation.PushAsync(new ReceiptPage(selectedItems, totalPrice));
+    }*/
+
 }
 
