cd848993cbbcbfcd3a7a0bc420bdc48b3b6cd3fb
TaiHPham <taihpham09@gmail.com>
2023-04-01 14:54:43 -0500

    Update Receipt btn navi & minor UI changes
    - Navigation.PushAsync(new MainPage());  creates a new instance of MainPage and pushes it onto the navigation stack, effectively replacing the existing page with a new one.
    
    - Fixed size frames. Size of frames does not change when resizing window anymore.

--- Lenny's Car Wash/ReceiptPage.xaml.cs
+++ Lenny's Car Wash/ReceiptPage.xaml.cs
@@ -22,6 +22,6 @@ public partial class ReceiptPage : ContentPage
 
     private void CreateNewOrder(object sender, EventArgs e)
     {
-        Navigation.PopAsync();
+        Navigation.PushAsync(new MainPage());
     }
 }
