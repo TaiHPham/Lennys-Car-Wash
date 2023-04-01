1bfc6ef03fb2f8ae61942c79f0cf1dbe7dfee497
TaiHPham <taihpham09@gmail.com>
2023-04-01 14:17:09 -0500

    Update
    Only thing left to figure out how to go to a cleared selection page.

--- Lenny's Car Wash/MainPage.xaml.cs
+++ Lenny's Car Wash/MainPage.xaml.cs
@@ -5,46 +5,68 @@ public partial class MainPage : ContentPage
 	public MainPage()
 	{
 		InitializeComponent();
-
-        submitBtn1.Clicked += CalcAmount;
     }
 
     private void CalcAmount(object sender, EventArgs e)
     {
         double totalCost = 0.00;
         bool isWashSelected = false;
-        string error = "Please select a wash";
+        bool isAddOnSelected = false;
+        string selectedWash = "";
+        List<string> selectedAddOns = new List<string>();
 
         if (rdoBasicWash.IsChecked == true)
         {
+            selectedWash = "Basic Wash ($10.00)";
             totalCost = 10.00;
             isWashSelected = true;
         }
         else if(rdoPremiumWash.IsChecked == true)
         {
+            selectedWash = "Premium Wash ($15.00)";
             totalCost = 15.00;
             isWashSelected = true;
         }
         else if(rdoUltraWash.IsChecked == true)
         {
+            selectedWash = "Ultra Wash ($20.00)";
             totalCost = 20.00;
             isWashSelected = true;
         }
 
         if (isWashSelected == false)
-            DisplayAlert("Error: ", error, "OK");
+            errorMsgLabel.Text = "Error: Please select a wash";
         else
         {
             if (chkAirFresh.IsChecked == true)
+            {
+                selectedAddOns.Add("• Air Freshener ($2.00)");
                 totalCost += 2.00;
+                isAddOnSelected = true;
+            }
             if (chkWax.IsChecked == true)
+            {
+                selectedAddOns.Add("• Wax ($5.00)");
                 totalCost += 5.00;
+                isAddOnSelected = true;
+            }
             if (chkShine.IsChecked == true)
+            {
+                selectedAddOns.Add("• Wheel Shine ($5.00)");
                 totalCost += 5.00;
+                isAddOnSelected = true;
+            }
             if (chkVacuum.IsChecked == true)
+            {
+                selectedAddOns.Add("• Vacuum ($2.00)");
                 totalCost += 2.00;
+                isAddOnSelected = true;
+            }
+            if (isAddOnSelected == false)
+                selectedAddOns.Add("⨯ No Add Ons were Selected");
 
-            ReceiptPage receiptPage = new ReceiptPage(totalCost);
+
+            ReceiptPage receiptPage = new ReceiptPage(totalCost, selectedWash, selectedAddOns);
             Navigation.PushAsync(receiptPage);
         }
     }
--- Lenny's Car Wash/ReceiptPage.xaml.cs
+++ Lenny's Car Wash/ReceiptPage.xaml.cs
@@ -1,11 +1,27 @@
+using System.Security;
+
 namespace Lenny_s_Car_Wash;
 
 public partial class ReceiptPage : ContentPage
 {
-	public ReceiptPage(double totalCost)
+	public ReceiptPage(double totalCost, string selectedWash, List<string> selectedAddOns)
 	{
 		InitializeComponent();
 
+        // Output to Text labels
         totalCostLabel.Text = "Total Cost: $" + totalCost.ToString("0.00###");
+        selectedWashLabel.Text = "Wash Selected: " + selectedWash;
+
+        string addOnsText = "";
+        foreach (string addOn in selectedAddOns)
+        {
+            addOnsText += addOn + "\n";
+        }
+        selectedAddOnsStack.Text = addOnsText;
+    }
+
+    private void CreateNewOrder(object sender, EventArgs e)
+    {
+        Navigation.PopAsync();
     }
 }
